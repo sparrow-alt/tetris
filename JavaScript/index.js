d4ac254457959c4e3fc6ba6e123698d91d2e2c71
sparrow-alt <97605916+sparrow-alt@users.noreply.github.com>
2023-03-28 22:00:15 +0000

    ga, safeguardss

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -7,10 +7,18 @@ function convert(input) {
     }
     return output;
   }
+
+function setCookie(cname, cvalue, exmin) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exmin*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 submitbutton.addEventListener("click", () => {
     let input = document.getElementById("input");
     let passwords = ["1100010 1100101 1100001 1110010 1110011 ", "1100010 1100101 1100001 1110010 "];
     if (passwords.includes(convert(input.value.toString().toLowerCase()))) {
+        setCookie("passcheck", "yes", 60);
         window.location.replace("https://sparrow-alt.github.io/tetris/mainindex.html");
     }else {
         let errors = ["nope", "wrong", "wrong wrong wrong", "nah", "no", "are you an impostor?", "lol no", "*dies inside* no"];
