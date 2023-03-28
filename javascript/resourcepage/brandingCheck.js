295492fd92724da83f7d3a238c624eb4146ae7c1
sparrow-alt <97605916+sparrow-alt@users.noreply.github.com>
2023-03-28 12:22:12 -0400

    Create brandingCheck.js

---
+++ javascript/resourcepage/brandingCheck.js
@@ -0,0 +1,23 @@
+$(function()
+{
+// 	var siteName = "eChalk";
+// 	var siteDisplayLink = "www.eChalk.co.uk";
+// 	var siteHref = "http://www.echalk.co.uk";
+	
+// 	if (window.location.host.indexOf("lumpty") != -1)
+// 	{
+// 		siteName = "Lumpty";
+// 		siteDisplayLink = "www.lumpty.com";
+// 		siteHref = "http://www.lumpty.com";
+// 	}
+	
+// 	// Cater for new and legacy by looking for siteLink both class and id
+// 	$('a#siteLink, a.siteLink').attr('href', siteHref);
+// 	$('#siteLink, .siteLink').text(siteDisplayLink);
+	
+// 	$('.siteName').text(siteName);
+	
+// 	// Set image alts to site name
+// 	$('img.siteLogoImage').attr('alt', siteName);
+  return;
+});
